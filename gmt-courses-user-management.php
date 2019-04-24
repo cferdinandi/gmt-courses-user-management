7388503e1686cf1d960a9c505bb0326fe8ce6003
cferdinandi <chris@gomakethings.com>
2019-04-24 16:03:24 -0400

    Fixed broken stuff

--- gmt-courses-user-management.php
+++ gmt-courses-user-management.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/cferdinandi/gmt-courses-user-management/
  * GitHub Plugin URI: https://github.com/cferdinandi/gmt-courses-user-management/
  * Description: User processes for GMT Courses.
- * Version: 0.6.0
+ * Version: 0.6.1
  * Author: Chris Ferdinandi
  * Author URI: http://gomakethings.com
  * License: GPLv3
