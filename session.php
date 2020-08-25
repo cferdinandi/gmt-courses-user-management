e61369d198b569c6526f054a311dd9d67f42a0ac
cferdinandi <chris@gomakethings.com>
2020-08-25 11:45:00 -0400

    Better session management

--- session.php
+++ session.php
@@ -1,12 +1,25 @@
 <?php
 
 	/**
+	 * Create session details
+	 * @return array The session details
+	 */
+	function gmt_courses_api_get_session_options () {
+		return array(
+			'cookie_lifetime' => 60 * 60 * 24 * 14, // 2 weeks
+			'gc_maxlifetime' => 60 * 60 * 24 * 14, // 2 weeks
+		);
+	}
+
+	/**
 	 * Start a new user session
 	 * @return string The session token
 	 */
-	function gmt_courses_api_start_session ($email) {
-		session_start();
-		$token = wp_generate_password(48, false);
+	function gmt_courses_api_start_session ($email, $token) {
+		session_start(gmt_courses_api_get_session_options());
+		if (empty($token)) {
+			$token = wp_generate_password(48, false);
+		}
 		$_SESSION['auth_email'] = $email;
 		$_SESSION['auth_token'] = $token;
 		$_SESSION['auth_token_last_access'] = time();
@@ -20,7 +33,7 @@
 	 * End a user session
 	 */
 	function gmt_courses_api_end_session () {
-		session_start();
+		session_start(gmt_courses_api_get_session_options());
 		unset($_SESSION['auth_email']);
 		unset($_SESSION['auth_token']);
 		unset($_SESSION['auth_token_last_access']);
@@ -31,9 +44,11 @@
 	 * @return boolean If true, session successfully extended
 	 */
 	function gmt_courses_api_extend_session () {
-		session_start();
-		if (empty($_SESSION['auth_token_last_access'])) return false;
-		$_SESSION['auth_token_last_access'] = time();
+		session_start(gmt_courses_api_get_session_options());
+		$email = $_SESSION['auth_email'];
+		$token = $_SESSION['auth_token'];
+		gmt_courses_api_end_session();
+		gmt_courses_api_start_session($email, $token);
 		return true;
 	}
 
@@ -50,7 +65,7 @@
 		}
 
 		// Check expiration
-		session_start();
+		session_start(gmt_courses_api_get_session_options());
 		if (empty($_SESSION['auth_token_last_access']) || ($_SESSION['auth_token_last_access'] + $expires) < time()) return true;
 		return false;
 
@@ -62,7 +77,7 @@
 	 * @return boolean        If true, the token is valid
 	 */
 	function gmt_courses_api_is_token_valid ($token) {
-		session_start();
+		session_start(gmt_courses_api_get_session_options());
 		if (!empty($token) && !empty($_SESSION['auth_token']) && strcmp($token, $_SESSION['auth_token']) === 0 && !gmt_courses_api_is_token_expired()) return true;
 		return false;
 	}
@@ -74,7 +89,7 @@
 	 * @return string        The user's email
 	 */
 	function gmt_courses_api_get_user_authenticated ($token) {
-		session_start();
+		session_start(gmt_courses_api_get_session_options());
 		if (empty(gmt_courses_api_is_token_valid($token)) || empty($_SESSION['auth_email'])) return false;
 		return $_SESSION['auth_email'];
 	}
@@ -85,9 +100,9 @@
 	 * @return boolean        If true, there's a current session
 	 */
 	function gmt_courses_api_is_authenticated ($token) {
-		session_start();
+		session_start(gmt_courses_api_get_session_options());
 		if (!empty($_SESSION['auth_email']) && !empty(gmt_courses_api_is_token_valid($token)) && !gmt_courses_api_is_token_expired()) {
-			$_SESSION['auth_token_last_access'] = time();
+			gmt_courses_api_extend_session();
 			return true;
 		}
 		return false;
@@ -98,7 +113,7 @@
 	 * @return string The user email
 	 */
 	function gmt_courses_api_get_user () {
-		session_start();
+		session_start(gmt_courses_api_get_session_options());
 		if (empty($_SESSION['auth_email']) || gmt_courses_api_is_token_expired()) return false;
 		return $_SESSION['auth_email'];
 	}
